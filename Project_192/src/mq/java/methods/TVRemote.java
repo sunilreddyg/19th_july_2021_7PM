7017429256ed83dd4c6b7edb3babbd8664c81a57
sunilreddyG1 <sunny.user2@rediffmail.com>
2021-07-29 20:19:53 +0530

    Java methods

---
+++ Project_192/src/mq/java/methods/TVRemote.java
@@ -0,0 +1,19 @@
+package mq.java.methods;
+
+public class TVRemote 
+{
+	
+
+	void ON()			//Reusable method
+	{
+		System.out.println("TV is ON");
+	}
+	
+	void OFF()			//Reusable method
+	{
+		System.out.println("TV is OFF");
+	}
+	
+	
+	
+}
